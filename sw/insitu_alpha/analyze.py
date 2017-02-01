6061adc6ba4a6a6b87b70110050cc41b35b24342
Peter Jansweijer <peterj@nikhef.nl>
2017-02-01 17:34:24 +0100

    detached SFP mudule specific code, analyze skips commented lines

--- sw/insitu_alpha/analyze.py
+++ sw/insitu_alpha/analyze.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python
 
 """
-tic_gui: Analyzes Time Interval Count Measurements and White Rabbit Abslolute Calibration GUI output
-
-This script calculates the differences between:
-- the time measured on the Time Interval Counter for PPS -> Tx/Rx-timestamp
-- the time stamped by the WR device (t1/t4) that was outputted on the WR GUI
-  while running the absolute calibration software.
-
-Measuremenrs should be taken while the WR device is in GrandMaster mode
-('mode gm') and locked to an external 10 MHz reference. Measurments are started
-after the 'ptp start' command
-
-The same reference clock mentioned above should also have been used for the Time Interval Counter measurement!
+analyze.py: Analyzes itu_channel/wavelength/crtt that was stored in a file
 
 Usage:
-  tic_gui.py     <crtt_file>
-  tic_gui.py     -h | --help
+  analyze.py     <crtt_file>
+  analyze.py     -h | --help
 
   <crtt_file>    <type 'str'> file name that contains the crtt measurements
                  for different SFP wavelengthes
@@ -39,16 +28,19 @@ import matplotlib.pyplot as plt
 
 # private imports:
 from lib.docopt import docopt
+import lib.wrt_sfppt015sc as tunable
 
 ############################################################################
 
-def file_to_scipy_array(filename):
+def file_to_dict(filename):
   """
   Retrieve the measurement file
    
   filename -- source file from which to retrieve data.
     
-  returns: <type 'numpy.ndarray'> measurements
+  returns: <type 'dict'>
+              key        : value
+              itu_channel: [wavelength, crtt]
   """
 
   # create an empty dictionairy
@@ -58,7 +50,7 @@ def file_to_scipy_array(filename):
   line = data_file.readline()
   #print(line)
   if line.strip() != "#In Situ Alpha measurements":
-    Exception("file_to_scipy_array: Not an In Situ Alpha measurements file.")
+    Exception("file_to_dict: Not an In Situ Alpha measurements file.")
     data_file.close()
     return (crtt_data)
 
@@ -70,33 +62,77 @@ def file_to_scipy_array(filename):
   #print(line)
 
   # create an empty lists for fields:
-  # channel, ITU channel, ITU wavelength [nm], crtt
-  lst_channel   = []
-  lst_itu_channel  = []
-  lst_itu_wavelength   = []
-  lst_crtt  = []
+  # sfp module channel, ITU channel, ITU wavelength [nm], crtt
 
   while 1:
     line=data_file.readline()
-    line_lst = line.split(",")
-    if len(line_lst) < 4:
-      break
+    if line[:len("#")]!="#":  # Skip lines that are commented out
+      line_lst = line.split(",")
+      if len(line_lst) < 4:
+        break
 
-    # Values one one line are:
-    # channel, ITU channel, ITU wavelength [nm], crtt
-    lst_channel.append(line_lst[0])
-    lst_itu_channel.append(line_lst[1])
-    lst_itu_wavelength.append(line_lst[2])
-    lst_crtt.append(line_lst[3])
+      # Values one one line are:
+      # channel, ITU channel, ITU wavelength [nm], crtt
+      lst_meas = []
+      lst_meas.append(line_lst[2])		# Wavelength
+      lst_meas.append(line_lst[3])		# crtt
+      lst_meas.append(line_lst[0])		# sfp module channel number
+      crtt_data.update({line_lst[1]:lst_meas})	# itu_channel: [wavelength, crtt]
 
   data_file.close()
 
-  crtt_data["channel"]=scipy.array(lst_channel)
-  crtt_data["itu_channel"]=scipy.array(lst_itu_channel)
-  crtt_data["itu_wavelength"]=scipy.array(lst_itu_wavelength)
-  crtt_data["crtt"]=scipy.array(lst_crtt)
+  #for key_itu_ch,value_itu_ch in sorted(crtt_data.items()):
+  #  print (key_itu_ch, value_itu_ch)
 
-  return crtt_data
+  return (crtt_data)
+
+############################################################################
+
+def dict_to_narray(crtt_data):
+  """
+  converts the data that is stored in crtt_data <type 'dict'> into 
+  <type 'numpy.ndarray'>
+   
+  returns: <type 'numpy.ndarray'>
+  """
+
+  crtt_array = {}
+  lst_channel = []
+  lst_itu_channel = []
+  lst_itu_wavelength = []
+  lst_crtt = []
+
+  for key_itu_ch,value_itu_ch in sorted(crtt_data.items()):
+    #print (key_itu_ch, value_itu_ch)
+    lst_itu_channel.append(key_itu_ch)
+    lst_itu_wavelength.append(value_itu_ch[0])
+    lst_crtt.append(value_itu_ch[1])
+    lst_channel.append(value_itu_ch[2])
+
+  crtt_array["itu_channel"]=scipy.array(lst_itu_channel)
+  crtt_array["itu_wavelength"]=scipy.array(lst_itu_wavelength)
+  crtt_array["crtt"]=scipy.array(lst_crtt)
+  crtt_array["channel"]=scipy.array(lst_channel)
+
+  return (crtt_array)
+
+############################################################################
+def calc_alpha(cl_wl, crtt_data, ref_itu_channel, fixed_itu_channel=20):
+  """
+  calc_alpha calculates the alpha factors for the wavelengths in the
+  crtt_data array of measurements.
+  It takes a ref_itu_channel for lambda 1 ans scans lambda 2 over each
+  other measurment.
+  A fixed wavelenth is used for either the forward or the backward
+  connection between master ans slave.
+  """
+  
+  tuneable_sfp_ch = tunable.sfp_channel(fixed_itu_channel)
+  wl = cl_wl[tuneable_sfp_ch][2]
+
+  print("fixed itu channel:",fixed_itu_channel, "= WRT-SFPPT015SC channel:", tuneable_sfp_ch, "wavelength: ", wl)
+
+  return(0)
 
 ############################################################################
 ##
@@ -112,17 +148,19 @@ if __name__ == "__main__":
   else:
     insitu_file = sys.argv[1]
 
-    crtt_data = file_to_scipy_array(insitu_file)
+    crtt_data = file_to_dict(insitu_file)
+    crtt_array = dict_to_narray(crtt_data)
 
-    #print(crtt_data["itu_channel"])
-    #print(crtt_data["crtt"])
+    #calc_alpha(cl_wl,crtt_data,11)
+    #sys.exit()
 
     fig = plt.figure("CRTT versus ITU Channel number")
     ax = fig.add_subplot(111)
     ax.set_xlabel('ITU Channel number')
     ax.set_ylabel('CRTT')
-    x = crtt_data["itu_channel"]
-    ax.plot(x,crtt_data["crtt"])
+    ax.text(0.01,0.95,str(insitu_file), transform=ax.transAxes)
+    x = crtt_array["itu_channel"]
+    ax.plot(x,crtt_array["crtt"])
     plt.draw()
     plt.show()
 
--- sw/insitu_alpha/lib/wrt_sfppt015sc.py
+++ sw/insitu_alpha/lib/wrt_sfppt015sc.py
@@ -6,7 +6,7 @@
 """
 
 ############################################################################
-def conv_ch(itu_channel):
+def sfp_channel(itu_channel):
   """
   converts the ITU channel number to the channel number used by the
   WRT-SFPPT015SC Tunable module
@@ -14,37 +14,15 @@ def conv_ch(itu_channel):
   return(int(itu_channel*2-21))
 
 ############################################################################
-def itu_2_frequency(itu_channel):
+def get_parameters():
   """
-  converts the ITU channel number to frequency
+  constant parameters for this particular tunable SFP module
   """
-  c = 299792458	 # speed of light 
-  itu_frequency = (190 + (itu_channel/10))*1e12
+  params = {}
+  params["vendor_id"] = "WRT-SFPPT015SC"
+  params["itu_channel_start"] = 11
+  params["itu_channel_stop"] = 61.5
+  params["itu_channel_spacing"] = 0.5
 
-  return (itu_frequency)
-
-############################################################################
-def itu_2_wavelength(itu_channel):
-  """
-  converts the ITU channel number to wavelength
-  """
-  c = 299792458	 # speed of light 
-  itu_wavelength = (c/itu_2_frequency(itu_channel))
-
-  return (itu_wavelength)
-
-###############################################
-def fill_channel_wavelength_dict():
-
-  c = 299792458	 # speed of light 
-  # WRT-SFPPT015SC channel, ITU Channel, ITU Frequency, ITU Wavelength
-  ch_wl = {}
-  for ch in range(1,103):	# selectable channel numbers WRT-SFPPT015SC (1..102)
-    ITU_ch = float((ch + 21)/2.0)
-    ITU_freq = (190 + (ITU_ch/10))*1e12
-    ITU_wl = (c/ITU_freq)
-    ch_wl_item = [ITU_ch,ITU_freq,ITU_wl]
-    ch_wl[ch]=ch_wl_item
-
-  return (ch_wl)
+  return (params)
 
