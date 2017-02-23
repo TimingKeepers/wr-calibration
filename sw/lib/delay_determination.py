ad372c0309ec6520ba328c0789882e0612ffbe47
Tjeerd Pinkert <t.j.pinkert@alumnus.utwente.nl>
2017-02-23 17:22:54 +0100

    Added GPLv3 licence to files

--- sw/lib/delay_determination.py
+++ sw/lib/delay_determination.py
@@ -1,6 +1,7 @@
 """
 Delay Determination Library with functions for delay estimation.
 
+-------------------------------------------------------------------------------
 Copyright (C) 2016  Tjeerd Pinkert
 
 This program is free software: you can redistribute it and/or modify
@@ -15,7 +16,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
-
 -------------------------------------------------------------------------------
 
 This library mainly holds closely inspected correlation functions which are
