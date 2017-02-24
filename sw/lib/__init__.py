9eab9b59fbe6e503cbf60b2edec2309744372743
Peter Jansweijer <peterj@nikhef.nl>
2017-02-24 11:48:47 +0100

    __init__.py copyrigth statement should be text!

--- sw/lib/__init__.py
+++ sw/lib/__init__.py
@@ -1,3 +1,4 @@
+'''
 -------------------------------------------------------------------------------
 Copyright (C) 2016 Peter Jansweijer
 
@@ -14,4 +15,4 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
 -------------------------------------------------------------------------------
-
+'''
